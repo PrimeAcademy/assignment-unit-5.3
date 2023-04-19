7735d37ffd6197b353362ffefa8347ae66f1c220
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-04-19 11:02:50 -0500

    finished findByArtist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,4 +1,4 @@
-console.log('***** Music Collection *****')
+console.log('***** Music Collection *****');
 
 // creating empty collection
 let collection = [];
@@ -33,7 +33,7 @@ console.log('The full collection is: ', collection);
 function showCollection(array) {
 
     // logging the number of albums
-    console.log(`The collection is ${array.length} albums.`)
+    console.log(`The collection is ${array.length} albums.`);
 
     // looping through each album, logging values with . operator
     for (let album of array) {
@@ -47,3 +47,25 @@ showCollection(collection);
 
 
 // start findByArtist
+function findByArtist(artist) {
+
+    // init empty array
+    let results = [];
+
+    // loop over each object in the collections array
+    for (val of collection) {
+
+        // if the artist property of the object = the input, add it to the results array
+        if (val.artist === artist) {
+            results.push(val);
+        }
+    }
+    //return the results array
+    return results;
+}
+//end findByArtist
+
+// findByArtist testing
+console.log('Testing findByArtist, expect 2 album objects: ', findByArtist('Metallica'));
+console.log('Testing findByArtist, expect 1 album object: ', findByArtist('Lumineers'));
+console.log('Testing findByArtist, expecting an empty array: ', findByArtist('Zach Bryan'));
