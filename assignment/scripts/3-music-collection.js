6afad9717f63e439356fbef1096d966e6d131c39
Luke Gartner <lukegartner4@gmail.com>
2023-04-18 20:29:38 -0500

    create addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,9 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+const addToCollection = (title, artist, yearPublished)=>{
+const addition = {title,artist,yearPublished};
+collection.push(addition);
+return addition;
+}
