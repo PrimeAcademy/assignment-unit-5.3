2350b13c51f5826bf13bead83fc6955e8fa248c5
Maya Peters <mayampeters@gmail.com>
2023-04-18 21:00:36 -0500

    created and tested showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -45,7 +45,16 @@ console.log(collection);
 //   - Console.log the number of items in the array.
 //   - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
 
+function showCollection(array) {
+    console.log(array.length);
+    for(let obj of array) {
+        console.log(`${obj.title} by ${obj.artist}, published in ${obj.year}`)
+    }
+}
+
 // - Test the `showCollection` function.
+
+showCollection(collection);
 
 // - Add a function named `findByArtist`. This function should:
 //   - Take in `artist` (a string) parameter
