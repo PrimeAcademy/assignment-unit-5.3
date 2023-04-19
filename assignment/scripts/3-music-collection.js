32eac19cd561817971aa8924e949163157f5c8a2
Hannah Engelhart <hannah.engelhart@gmail.com>
2023-04-19 15:40:41 -0500

    created first function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollenction(title, artist, yearPublished) {
+    this.title = title;
+    this.artist = artist;
+    this.yearPublished = yearPublished;
+    collection.push(this);
+    return this;
+}
+
+
