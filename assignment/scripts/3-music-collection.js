194c923a78bef45a314022074cee34d5b7fd71cc
Luke Gartner <lukegartner4@gmail.com>
2023-04-18 20:39:32 -0500

    test addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,9 +1,20 @@
 console.log('***** Music Collection *****')
 
+// collection array
 let collection = [];
 
+// add to collection function
 const addToCollection = (title, artist, yearPublished)=>{
 const addition = {title,artist,yearPublished};
 collection.push(addition);
 return addition;
 }
+// test addToCollection
+console.log("add to collection:", addToCollection("Carnavas", "Silversun Pickups", 2006));
+console.log("add to collection:", addToCollection("The Raven That Refused To Sing (And Other Stories)", "Steven Wilson", 2013));
+console.log("add to collection:", addToCollection("Swoon", "Silversun Pickups", 2009));
+console.log("add to collection:", addToCollection("Hand. Cannot. Erase.", "Silversun Pickups", 2015));
+console.log("add to collection:", addToCollection("Maps of Non-Existent Places", "Thank You Scientist", 2013));
+console.log("add to collection:", addToCollection("Revolver", "The Beetles", 1966));
+
+console.log("collection:", collection);
