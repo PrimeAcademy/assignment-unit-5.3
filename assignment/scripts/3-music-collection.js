2bccbc2a505728c92357fe5fd3005678ea689580
Hannah Engelhart <hannah.engelhart@gmail.com>
2023-04-20 13:50:39 -0500

    adjusted testing albums to include siilarities

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -17,5 +17,5 @@ let album2 = new addToCollection('Recovery', 'Eminem', 2010);
 let album3 = new addToCollection('Last Young Renegade', 'All Time Low', 2017);
 let album4 = new addToCollection('Songs About Stuff', 'Nur-D', 2019);
 let album5 = new addToCollection('Disconnect', 'Phantoms', 2019);
-let album6 = new addToCollection('In Return', 'Odesza', 2014);
+let album6 = new addToCollection('Trapped in My Room', 'Nur-D', 2020);
 console.log('Collection:', collection);
