5b4b2eb1d6daae1954163a964e7632da30b52f7a
Justin Reichelt <justin.j.reichelt@gmail.com>
2023-04-20 11:54:03 -0400

    created collection variable as empty array, function, and obect array with properties for first task.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+
+function addToCollection(title, artist, yearPublished) {
+let album = {
+title: 'LoFi',
+artist: 'PapaSmurf',
+yearPublished: 2023
+};
+collection.push(album);
+return album
+}
