67acc8d6a795cb221a10657887520c7c83e932b1
Hannah Engelhart <hannah.engelhart@gmail.com>
2023-04-20 13:44:23 -0500

    added albums to the array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,12 +2,20 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollenction(title, artist, yearPublished) {
-    this.title = title;
-    this.artist = artist;
+function addToCollection(title, artist, yearPublished) {
+    // console.log(this);
+    this.title = title,
+    this.artist = artist,
     this.yearPublished = yearPublished;
     collection.push(this);
+    // console.log(this);
     return this;
 }
 
-
+let album1 = new addToCollection('BB U OK?', 'San Holo', 2021);
+let album2 = new addToCollection('Recovery', 'Eminem', 2010);
+let album3 = new addToCollection('Last Young Renegade', 'All Time Low', 2017);
+let album4 = new addToCollection('Songs About Stuff', 'Nur-D', 2019);
+let album5 = new addToCollection('Disconnect', 'Phantoms', 2019);
+let album6 = new addToCollection('In Return', 'Odesza', 2014);
+console.log('Collection:', collection);
