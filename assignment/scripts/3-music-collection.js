94765a9a0f683ef68d34d7641dfcc8cc236b3fb3
Andreas Copan <avcopan@gmail.com>
2023-04-22 17:37:26 -0400

    Implements nice collection logging

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,9 @@
 console.log('***** Music Collection *****');
 
+// global variabls
 let collection = [];
 
+// functions
 let addToCollection = (title, artist, yearPublished) => {
     let album = {
         title: title,
@@ -12,6 +14,17 @@ let addToCollection = (title, artist, yearPublished) => {
     return album;
 };
 
+let showCollection = (albumArray) => {
+    console.log(`Number of albums: ${albumArray.length}`);
+    for (let album of albumArray) {
+        console.log(
+            `${album.title} by ${album.artist},` +
+                ` released in ${album.yearPublished}`
+        );
+    }
+};
+
+// main
 console.log('Before adding:', collection);
 
 addToCollection('Emotions and Math', 'Margaret Glaspy', 2016);
@@ -25,5 +38,12 @@ addToCollection('Cop and Speeder', 'Heatmiser', 1992);
 addToCollection('John Prine', 'John Prine', 1971);
 addToCollection('Sweet Revenge', 'John Prine', 1973);
 addToCollection('Bruised Orange', 'John Prine', 1978);
+addToCollection('In My Own Time', 'Karen Dalton', 1971);
+addToCollection('The Dreaming', 'Kate Bush', 1982);
+addToCollection('LUSH', 'Mitski', 2012);
+addToCollection('Puberty 2', 'Mitski', 2016);
+addToCollection('Be the Cowboy', 'Mitski', 2018);
 
 console.log('After adding:', collection);
+
+showCollection(collection);
