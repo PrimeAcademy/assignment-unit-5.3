c44e02eb7b92b3abcfdbf73607d1c63bf05268a2
Michael Duren <michaeld@michaelduren.com>
2023-04-22 09:51:04 -0500

    added data to collection and tested

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,21 +1,31 @@
-console.log('***** Music Collection *****')
+console.log('***** Music Collection *****');
 
 const collection = [];
 
 const addToCollection = (title, artist, yearPublished) => {
-		const album = {
-				title,
-				artist,
-				yearPublished
-		}
-		collection.push(album);
-		return album;
-}
+  const album = {
+    title,
+    artist,
+    yearPublished,
+  };
+  collection.push(album);
+  return album;
+};
 
 const showCollection = (collectionArray) => {
-	console.log(collectionArray.length);
-	collectionArray.forEach((album) => {
-		console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
-	}
-	)
-}
+  console.log(collectionArray.length);
+  collectionArray.forEach((album) => {
+    console.log(
+      `${album.title} by ${album.artist}, published in ${album.yearPublished}`
+    );
+  });
+};
+
+// add content to collection
+addToCollection('Tonight, Tonight', 'The Smashing Pumpkins', 1995);
+addToCollection('Fake Plastic Trees', 'Radiohead', 1995);
+addToCollection('Smells Like Teen Spirit', 'Nirvana', 1991);
+addToCollection('Fight The Power', 'Public Enemy', 1989);
+
+// test showCollection
+showCollection(collection);
