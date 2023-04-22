6fee3d171d54e58aceba692b94f2afa58c706517
Michael Karas <michael_primeacademy@karas1.com>
2023-04-22 08:26:27 -0400

    starting to put a function together

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,10 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
+
+collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  collection.push(title, artist, yearPublished);
+  console.log(collection);
+}
+
+addToCollection("Little Sunshine", "Ghost", 2022);
