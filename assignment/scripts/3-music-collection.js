0992d8052bb7a97deafb5e2d3d5cbb7f90c26ba9
Michael Duren <michaeld@michaelduren.com>
2023-04-22 11:16:42 -0500

    updated album objects to include track listing

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,11 +2,12 @@ console.log('***** Music Collection *****');
 
 const collection = [];
 
-const addToCollection = (title, artist, yearPublished) => {
+const addToCollection = (title, artist, yearPublished, tracks) => {
   const album = {
     title,
     artist,
     yearPublished,
+    tracks,
   };
   collection.push(album);
   return album;
@@ -16,21 +17,67 @@ const showCollection = (collectionArray) => {
   console.log(collectionArray.length);
   collectionArray.forEach((album) => {
     console.log(
-      `${album.title} by ${album.artist}, published in ${album.yearPublished}`
+      `\n${album.title} by ${album.artist}, published in ${album.yearPublished}`
+    );
+    console.log('*'.repeat(60));
+    console.log();
+    album.tracks.forEach((track, i) =>
+      console.log(`${i + 1}. ${track.name} ${track.duration}`)
     );
   });
 };
+
+const mellonCollieTracks = [
+  { name: 'Mellon Collie and the Infinite Sadness', duration: '2:52' },
+  { name: 'Tonight, Tonight', duration: '4:14' },
+  { name: 'Jellybelly', duration: '3:01' },
+  { name: 'Zero', duration: '2:40' },
+];
+
+const theBendsTracks = [
+  { name: 'Planet Telex', duration: '4:19' },
+  { name: 'The Bends', duration: '4:06' },
+  { name: 'High and Dry', duration: '4:17' },
+  { name: 'Fake Plastic Trees', duration: '4:50' },
+];
+
+const kidATracks = [
+  { name: "Everything In It's Right Place", duration: '4:11' },
+  { name: 'Kid A', duration: '4:44' },
+  { name: 'The National Anthem', duration: '5:51' },
+  { name: 'How to Dissapear Completely', duration: '5:56' },
+];
+
+const nevermindTracks = [
+  { name: 'Smells Like Teen Spirit', duration: '5:01' },
+  { name: 'In Bloom', duration: '4:14' },
+  { name: 'Come As You Are', duration: '3:39' },
+  { name: 'Breed', duration: '3:03' },
+];
+
+const fearOfABlackPlanetTracks = [
+  { name: 'Contract on the World Love Jam', duration: '1:44' },
+  { name: 'Brothers Gonna Work It Out', duration: '5:07' },
+  { name: '911 Is a Joke', duration: '3:17' },
+  { name: 'Incident at 66.6 FM', duration: '1:37' },
+];
 
 // add content to collection
 addToCollection(
   'Mellon Collie and the Infinite Sadness',
   'The Smashing Pumpkins',
-  1996
+  1996,
+  mellonCollieTracks
 );
-addToCollection('The Bends', 'Radiohead', 1995);
-addToCollection('Kid A', 'Radiohead', 2001);
-addToCollection('Nevermind', 'Nirvana', 1992);
-addToCollection('Fear of a Black Planet', 'Public Enemy', 1990);
+addToCollection('The Bends', 'Radiohead', 1995, theBendsTracks);
+addToCollection('Kid A', 'Radiohead', 2001, kidATracks);
+addToCollection('Nevermind', 'Nirvana', 1992, nevermindTracks);
+addToCollection(
+  'Fear of a Black Planet',
+  'Public Enemy',
+  1990,
+  fearOfABlackPlanetTracks
+);
 
 // test showCollection
 showCollection(collection);
@@ -65,6 +112,7 @@ const search = (searchObj = { artist: '', year: '' }) => {
   }
 };
 
+// search tests
 const publicEnemy1990Search = search({ artist: 'Public Enemy', year: 1990 });
 console.log('Should log one album');
 console.log(publicEnemy1990Search);
