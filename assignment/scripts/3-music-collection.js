baec614017cf9c7f521a72ca0d7297424da56288
Michael Karas <michael_primeacademy@karas1.com>
2023-04-22 13:14:01 -0400

    thunderstorm rolling in, saving and shutting down

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,5 +11,20 @@ function addToCollection(title, artist, yearPublished) {
   collection.push(tempCollection);
   console.log(collection);
 }
+console.log(collection);
 
 addToCollection("Little Sunshine", "Ghost", 2022);
+addToCollection("Holy Roller", "Spirit Box", 2020);
+addToCollection("Pices", "Jinjer", 2016);
+addToCollection("Cold Like War", "We Came As Romans", 2017);
+addToCollection("Voices in My Head", "Falling in Reverse", 2022);
+addToCollection("Toxic", "Lauren Babic", 2019);
+
+function showCollection() {
+  console.log(collection.length);
+  for (let song of collection) {
+    console.log(collection[song].yearPublished);
+  }
+}
+
+showCollection();
