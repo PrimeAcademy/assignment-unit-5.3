7674b20eabd2b13b9423f412fedcf66d32a56a3e
Halima Omar <omarhalima100@gmail.com>
2023-04-22 23:35:53 -0500

    created a empty array named collection, created function namedd addToCollection with parameter title, artisit, and yearpublished. crated record object and added it to the end of coolection array.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+let collection = [];
+console.log(collection);
+
+function addToCollection(title, artist, yearPublished) {
+let records = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+};
+collection.push(records);
+return records;
+}
+
