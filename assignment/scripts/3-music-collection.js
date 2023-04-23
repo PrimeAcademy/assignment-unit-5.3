b69144b5e84ae17129f6ba5d05674f0749312a4e
Halima Omar <omarhalima100@gmail.com>
2023-04-22 23:53:48 -0500

    added 6 albums to my collection then logged each album and lastly logged the collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,3 +12,16 @@ collection.push(records);
 return records;
 }
 
+let albumeOne = addToCollection("ctrl", "SZA", "2017");
+console.log(albumeOne);
+let albumeTwo = addToCollection("The Miseducation of Lauryn Hill", "Lauryn Hill", "1998");
+console.log(albumeTwo);
+let albumeThree = addToCollection("SOS", "SZA", "2022");
+console.log(albumeThree);
+let albumeFour = addToCollection("Whats going on", "Marvin Gaye", "1971");
+console.log(albumeFour);
+let albumeFive = addToCollection("Blond", "Frank Ocean", "2017");
+console.log(albumeFive);
+let albumSix = addToCollection("Off the Wall", "Michael Jackson", "1979");
+console.log(albumSix);
+console.log(collection);
