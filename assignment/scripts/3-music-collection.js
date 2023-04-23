acf62a998a15740c7f0369d7ffcb8e3ba012c8b8
Javier Navarro <javierna182@gmail.com>
2023-04-23 11:19:38 -0500

    completed showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -22,24 +22,28 @@ console.log('New album added:', addToCollection('Dookie', 'Green Day', '1994'));
 console.log('New album added:', addToCollection('Meteora', 'Linkin Park', '2003'));
 console.log('New album added:', addToCollection('Hot Fuss', 'The Killers', '2004'));
 
-console.log(collection);
+console.log('My album collection:',collection);
 
+// - Add a function named `showCollection`. This function should:
+//   - Take in an array parameter. (This allows it to be reused to show any collection, like 
+// the results from the find or search.)
+function showCollection(albumArray){
+//   - Console.log the number of items in the array.
+    console.log(albumArray.lenght)
+    
+    for (newAlbum of albumArray){
+        console.log(`${newAlbum.title} by ${newAlbum.title} in ${newAlbum.title}`);
+    }
+    return albumArray;
+  
 
+//   - Console.log the number of items in the array.
+//   - Loop over the array and console.log each album's information formatted like: 
+// `TITLE by ARTIST, published in YEAR`.
 
-// - Create a variable `collection` that starts as an empty array.
+};
+console.log(showCollection(collection)); 
 
-// - Add a function named `addToCollection`. This function should:
-//   - Take in the album's `title`, `artist`, `yearPublished` as input parameters
-//   - Create a new object having the above properties
-//   - Add the new object to the end of the `collection` array
-//   - Return the newly created object
-
-// - Test the `addToCollection` function:
-//   - Add 6 albums to your collection. Aim to have a mix of both same and different artists
-//  and published years. (Feel free to share your musical interests, 
-//     or make stuff up. Totally fine either way.)
-//   - Console.log each album as added using the returned value.
-//   - After all are added, console.log the `collection` array.
 
 // - Add a function named `showCollection`. This function should:
 //   - Take in an array parameter. (This allows it to be reused to show any collection, like 
