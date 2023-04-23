0c50f8fd03fa812d7e0e5303406d65f298d89259
Halima Omar <omarhalima100@gmail.com>
2023-04-23 01:10:37 -0500

    created showCollection function, added array parameter, looped over the aray and logged each albums info

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,16 +12,26 @@ collection.push(records);
 return records;
 }
 
-let albumeOne = addToCollection("ctrl", "SZA", "2017");
-console.log(albumeOne);
-let albumeTwo = addToCollection("The Miseducation of Lauryn Hill", "Lauryn Hill", "1998");
-console.log(albumeTwo);
-let albumeThree = addToCollection("SOS", "SZA", "2022");
-console.log(albumeThree);
-let albumeFour = addToCollection("Whats going on", "Marvin Gaye", "1971");
-console.log(albumeFour);
-let albumeFive = addToCollection("Blond", "Frank Ocean", "2017");
-console.log(albumeFive);
+let albumOne = addToCollection("ctrl", "SZA", "2017");
+console.log(albumOne);
+let albumTwo = addToCollection("The Miseducation of Lauryn Hill", "Lauryn Hill", "1998");
+console.log(albumTwo);
+let albumThree = addToCollection("SOS", "SZA", "2022");
+console.log(albumThree);
+let albumFour = addToCollection("Whats going on", "Marvin Gaye", "1971");
+console.log(albumFour);
+let albumFive = addToCollection("Blond", "Frank Ocean", "2017");
+console.log(albumFive);
 let albumSix = addToCollection("Off the Wall", "Michael Jackson", "1979");
 console.log(albumSix);
 console.log(collection);
+
+function showCollection(array) {
+ console.log(`Number of items in the array ${array}`);   
+for (let myArray of array) {
+    console.log(`${myArray.title} by ${myArray.artist} in ${myArray.yearPublished}`);
+}
+}
+showCollection((collection));
+
+
