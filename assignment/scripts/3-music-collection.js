72bfe773f27c6c4a2b3063d61db13205e4297a41
Daniel Davidson <danielmcraedavidson@gmail.com>
2023-04-25 16:51:03 -0500

    modified addtoCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,69 +1,81 @@
 console.log('***** Music Collection *****')
 
-// Created a variable called collection and wrote
-// the function addTo Collection
+// Created a variable called collection and
+// the function addToCollection
 
 let collection = [];
 
-function addToCollection (album) {
-    collection.push(album);
-    return album;
+function addToCollection (title, artist, year) {
+    let album = {
+        title,
+        artist,
+        yearPublished: year,
+    };
+    collection.push(album)
+    return collection;
 }
 
-// Created objects for each album with their properties
+// let album1 = {
+//     title: 'Hopes And Fears',
+//     artist: 'Keane',
+//     yearPublished: '2004',
+//     tracks: ['Somewhere Only We Know', 'Everybody\'s Changing', 'This Is the Last Time']
+// }
 
-const album1 = {
-    title: 'Hopes And Fears',
-    artist: 'Keane',
-    yearPublished: '2004'
-}
-
-const album2 = {
-    title: 'A Thousand Suns',
-    artist: 'Linkin Park',
-    yearPublished: '2010'
-}
-const album3 = {
-    title: 'How To Conquer A Land',
-    artist: 'And The Golden Choir',
-    yearPublished: '2017'
-}
-const album4 = {
-    title: 'Riot!',
-    artist: 'Paramore',
-    yearPublished: '2007'
-}
-const album5 = {
-    title: 'Hot Fuss',
-    artist: 'The Killers',
-    yearPublished: '2004'
-}
-const album6 = {
-    title: 'Minutes to Midnight',
-    artist: 'Linkin Park',
-    yearPublished: '2007',
-}
+// let album2 = {
+//     title: 'A Thousand Suns',
+//     artist: 'Linkin Park',
+//     yearPublished: '2010',
+//     tracks: ['Burning In the Skies', 'Robot Boy', 'Waiting for the End']
+// }
+// let album3 = {
+//     title: 'How To Conquer A Land',
+//     artist: 'And The Golden Choir',
+//     yearPublished: '2017',
+//     tracks: {
+//         Name:'How to Conquer A Land',
+//         Duration: '3:44'
+//     }
+// }
+// let album4 = {
+//     title: 'Riot!',
+//     artist: 'Paramore',
+//     yearPublished: '2007',
+//     tracks: ['That\'s What You Get', 'Misery Business', 'crushcrushcrush']
+// }
+// let album5 = {
+//     title: 'Hot Fuss',
+//     artist: 'The Killers',
+//     yearPublished: '2004',
+//     tracks: ['Mr. Brightside', 'Smile Like You Mean It']
+// }
+// let album6 = {
+//     title: 'Minutes to Midnight',
+//     artist: 'Linkin Park',
+//     yearPublished: '2007',
+//      tracks: ['What I\'ve Done', 'Shadow of the Day', 'In Pieces']
+// }
 
 // Added each album to collection. Logging each one in connsole and then
 // logging the collection array.
 
-addToCollection(album1);
-console.log(album1);
+addToCollection("Hopes And Fears", "Keane", "2004");
+console.log(collection[0]);
 
-addToCollection(album2);
-console.log(album2);
+addToCollection("A Thousand Suns", "Linkin Park", "2010");
+console.log(collection[1]);
 
-addToCollection(album3);
-console.log(album3);
+addToCollection("How To Conquer A Land", "And The Golden Choir", "2017");
+console.log(collection[2]);
 
-addToCollection(album4);
-console.log(album4);
+addToCollection("Riot!", "Paramore", "2007");
+console.log(collection[3]);
 
-addToCollection(album5);
-console.log(album5);
+addToCollection("Hot Fuss", "The Killers", "2004");
+console.log(collection[4]);
 
-addToCollection(album6);
-console.log(album6);
+addToCollection("Minutes to Midnight", "Linkin Park", "2007");
+console.log(collection[5]);
 
 console.log(collection);
 
@@ -76,11 +88,11 @@ function showCollection(collection) {
             console.log(album.title + " by " + album.artist + ", published in " + album.yearPublished);
     }
 }
-// Tested function showCollection
+// // Tested function showCollection
 
 showCollection(collection);
 
-// Created function findByArtist
+// // Created function findByArtist
 
 function findByArtist(artist) {
     let results = [];
@@ -97,28 +109,27 @@ console.log(findByArtist("Linkin Park"));
 console.log(findByArtist("Paramore"));
 console.log(findByArtist("Coldplay"));
 
-// Stretch goals
+// // Stretch goals
 
-// Created search function
+// // Created search function
 
-function search (artist, year) {
+function search (item) {
     let searchResults = [];
     for (let album of collection) {
-        if (album.artist === artist && album.yearPublished === year) {
+        if (album.artist === item || album.yearPublished === item) {
             searchResults.push("artist: " + album.artist + ", year: " + album.yearPublished);
+        } else {
+            searchResults;
         }
     }
-    if (artist || year) {
-            searchResults;
-    }
-    if (!artist && !year) {
+    if (!item) {
             searchResults.push(collection);
         }
     return searchResults;
 }
 
 // Testing search function
-console.log(search("Linkin Park", "2010"));
+console.log(search("Linkin Park"));
 console.log(search("Keane"));
 console.log(search("2000"));
 console.log(search(""));
