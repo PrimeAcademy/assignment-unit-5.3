d1468835e7d75600eec95e057087828a17c43c53
Gabrielle Lepensky <gabrielle.lepensky@gmail.com>
2023-04-26 01:29:31 -0500

    created collection and addtocollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,18 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    return album
+}
+
+let album = {title:'Saves the World', artist: 'Muna', yearPublished: 2019};
+
+collection.push(album);
+console.log(collection);
+
+
+
+
+
+
+
