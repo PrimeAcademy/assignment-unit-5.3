05da3acea458bb82ddd108e79cf14ef95900465f
Gabrielle Lepensky <gabrielle.lepensky@gmail.com>
2023-05-09 15:00:28 -0500

    working until line 45

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,18 +1,48 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
 let collection = [];
+// Created collection variable of empty array.
 
 function addToCollection(title, artist, yearPublished) {
-    return album
+  let records = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  };
+  collection.push(records);
+  return records;
 }
+// Created function named addToCollection that takes in a record's keys as parameters.
+// Created new variable records in the function with these properties.
+// Added new records object to the collection array.
+// Return records.
 
-let album = {title:'Saves the World', artist: 'Muna', yearPublished: 2019};
+let albumOne = addToCollection("Chime", "Dessa", 2018);
+console.log(albumOne);
+let albumTwo = addToCollection("Preachers Daughter", "Ethel Cain", 2022);
+console.log(albumTwo);
+let albumThree = addToCollection("Michigan", "Sufjan Stevens", 2003);
+console.log(albumThree);
+let albumFour = addToCollection("Carrie & Lowell", "Sufjan Stevens", 2015);
+console.log(albumFour);
+let albumFive = addToCollection("No Shape", "Perfume Genius", 2017);
+console.log(albumFive);
+let albumSix = addToCollection(
+  "Because the Internet",
+  "Childish Gambino",
+  2015
+);
+console.log(albumSix);
+// Added 6 albums to collection.
 
-collection.push(album);
 console.log(collection);
+//console.log collection array.
 
-
-
-
-
-
-
+function showCollection(array) {
+  console.log(`Number of items: ${array.length}`);
+  for (i = 0; i < array.length; i++) {
+   let item = array[i];
+   console.log(item);
+//     console.log(`${title} by ${artist}, published in ${yearPublished}`);
+   }
+}
+console.log(showCollection(collection));
